5343a9b91c7e5dd180f88f04b0464a59f1f3c3a2
ivankosovan <ivn.kosovan@sapient.pro>
2025-10-03 10:40:25 +0200

    session: update session driver, add session middleware

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Session\Middleware\StartSession;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\CompareController;
@@ -15,9 +16,11 @@ Route::apiResource('categories', CategoryController::class);
 
 Route::get('/top10Products', [ProductController::class, 'top10'])->name('products.top10');
 
-Route::prefix('compare')->group(function() {
-    Route::get('/', [CompareController::class, 'index']);
-    Route::post('add', [CompareController::class, 'add']);
-    Route::delete('{id}', [CompareController::class, 'remove']);
-    Route::delete('clear', [CompareController::class, 'clear']);
-});
+Route::prefix('compare')
+    ->middleware([StartSession::class])
+    ->group(function() {
+        Route::get('/', [CompareController::class, 'index']);
+        Route::post('add', [CompareController::class, 'add']);
+        Route::delete('{id}', [CompareController::class, 'remove']);
+        Route::delete('clear', [CompareController::class, 'clear']);
+    });
